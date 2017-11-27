fbec15540ab1d2f11f4af229cb093dab788e7c9c
erikwirenhed <erik.wirenhed.te16d@gmail.com>
2017-11-27 16:09:44 +0100

    jag börjar med att skapa en scanner så att jag kan ge mina input till uppgiften.

--- src/uppgifter/FizzBuzz.java
+++ src/uppgifter/FizzBuzz.java
@@ -1,5 +1,14 @@
 package uppgifter;
 
+import java.util.Scanner;
+
 public class FizzBuzz {
+ public static void main(String[] args) {
+	
+	 Scanner sc = new Scanner(System.in); 
+	 
+	 
+	 
+}
 
 }
