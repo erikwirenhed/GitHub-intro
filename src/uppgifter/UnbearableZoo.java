dbe9b96cc7dc4dfb101dcff1d12788788ad8c861
erikwirenhed <erik.wirenhed.te16d@gmail.com>
2017-11-27 22:44:28 +0100

    detta är en loop som tar in en sträng och lagrar det sista av ordet i meningen i strängen last.

---
+++ src/uppgifter/UnbearableZoo.java
@@ -0,0 +1,21 @@
+package uppgifter;
+
+import java.util.Scanner;
+
+public class UnbearableZoo {
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		int a = sc.nextInt();
+		
+				
+		for (int i = 0; i < a; i++) {
+			String c = sc.nextLine();
+			
+			String last = c.substring(c.lastIndexOf(" ")+1);
+			
+		}
+		
+	}
+}
